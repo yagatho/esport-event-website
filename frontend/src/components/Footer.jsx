e3859a47b98895a5d2dcc8a7f5f77e133d1c2fbb
wrzepka <rzepka.wiktor1@gmail.com>
2025-05-12 17:53:24 +0200

    feat: Create site Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer className="bg-[#1E1E1E] w-full py-6 flex flex-col items-center justify-center text-center text-[#A0A0A0] space-y-2">
+            <h1 className="font-bold">ŁÓDZKIE GRAJĄCE ©</h1>
+            <h2>Projekt na "Projektowanie interfejsów aplikacji www"</h2>
+            <h3 className="mt-2">Twórcy:</h3>
+            <div className="flex gap-4">
+                <a href={"https://github.com/yagatho"} className="hover:text-[#30E9EE] transition-colors">Szymon Grucela</a>
+                <a href={"https://github.com/wrzepka"} className="hover:text-[#30E9EE] transition-colors">Wiktor Rzepka</a>
+            </div>
+        </footer>
+    )
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './styles/index.css'
 import Menu from './components/Menu.jsx'
+import Footer from './components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <Menu/>
+        <Footer/>
     </StrictMode>,
 )
